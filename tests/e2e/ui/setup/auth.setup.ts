a1aed6c8542a676d0bbad12ab1be6ba3c4ea3e35
Thomas <przegralek.thomas@gmail.com>
2025-03-23 11:25:55 +0100

    Suppression des fichiers inutiles du dépôt

--- tests/e2e/ui/setup/auth.setup.ts
+++
@@ -1,7 +0,0 @@
-import {test as setup} from '../../fixtures/base.fixture'
-
-setup('Connexion avec un utilisateur standard', async ({sidebar}) => {
-    await sidebar.goTo('parabank/index.htm')
-    await sidebar.fillLoginForm(process.env.USERNAME, process.env.PASSWORD)
-    await sidebar.clickLogin()
-})
