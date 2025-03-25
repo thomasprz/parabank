253a1532d5d63bc188438733e34e9e8274f21acf
Thomas <przegralek.thomas@gmail.com>
2025-03-25 22:22:45 +0100

    Ajout du bouton 'Open New Account' et amélioration des méthodes dans OpenAccountPage

--- tests/pages/open-account.page.ts
+++ tests/pages/open-account.page.ts
@@ -6,7 +6,7 @@ export class OpenAccountPage extends BasePage {
     readonly locatorOpenAccountTitle : Locator
     readonly locatorTypeAccount : Locator
     readonly locatorTransferFunds : Locator
-    //
+    readonly locatorOpenNewAccountButton : Locator
     readonly locatorAccountOpenedMessage : Locator
     readonly locatorNewAccountId : Locator
 
@@ -16,8 +16,7 @@ export class OpenAccountPage extends BasePage {
         this.locatorOpenAccountTitle = page.getByRole('heading', {name:'Open New Account'})
         this.locatorTypeAccount = page.locator('#type')
         this.locatorTransferFunds = page.locator('#fromAccountId')
-
-        //
+        this.locatorOpenNewAccountButton = page.locator('[value="Open New Account"]')
         this.locatorAccountOpenedMessage = page.getByRole('heading', {name:'Account Opened!'})
         this.locatorNewAccountId = page.locator('#newAccountId')
     }
@@ -25,22 +24,27 @@ export class OpenAccountPage extends BasePage {
     
     async expectOpenAccountPage(){
         await expect(this.page).toHaveURL(/openaccount/)
+        await expect(this.locatorOpenAccountTitle).toBeVisible()
     }
 
-    async selectAccountType(account){
-        await this.locatorTypeAccount.selectOption(account)
-    } 
+    async selectAccountType(type) {
+        await this.locatorTypeAccount.selectOption({ label: type });
+    }
 
     async selectAccountTransferFrom(account){
         await this.locatorTransferFunds.selectOption(account)
     } 
 
-    async accountCreatedId(){
-        const accountId = await this.locatorNewAccountId.innerText()
-        return accountId
+    async clickOpenNewAccount(){
+        await this.locatorOpenNewAccountButton.click()
     }
 
     async clickAccountCreated(id){
         await this.locatorNewAccountId.click()
+    }
+
+    async getAccountCreatedId(){
+        const accountId = await this.locatorNewAccountId.innerText()
+        return accountId
     }
 }
