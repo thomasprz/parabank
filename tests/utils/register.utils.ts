2f1f0d45c4e2963b303e2de0cd2ce0980795a392
Thomas <przegralek.thomas@gmail.com>
2025-03-25 22:14:35 +0100

    Réorganiser le code pour améliorer la lisibilité de la fonction registerDataFaker

--- tests/utils/register.utils.ts
+++ tests/utils/register.utils.ts
@@ -1,16 +1,16 @@
 import { faker } from "@faker-js/faker";
 
-export function registerDataFaker(){
-    return{
-        firstname: faker.person.firstName(),
-        lastname: faker.person.lastName(),
-        address: faker.location.streetAddress(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zipcode: faker.location.zipCode(),
-        phone: faker.phone.number(),
-        ssn: faker.helpers.replaceSymbols('###-##-####'),
-        username: faker.internet.username(),
-        password: faker.internet.password(),
-    }
+export function registerDataFaker() {
+  return {
+    firstname: faker.person.firstName(),
+    lastname: faker.person.lastName(),
+    address: faker.location.streetAddress(),
+    city: faker.location.city(),
+    state: faker.location.state(),
+    zipcode: faker.location.zipCode(),
+    phone: faker.phone.number(),
+    ssn: faker.helpers.replaceSymbols('###-##-####'),
+    username: faker.internet.username(),
+    password: faker.internet.password()
+  };
 }
