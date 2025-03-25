2eb8870d34f3e10babadbc079975fe7daf76e8fc
Thomas <przegralek.thomas@gmail.com>
2025-03-25 22:25:10 +0100

    Ajout de la classe AccountsOverviewPage pour gérer l'aperçu des comptes

--- tests/pages/accounts-overview.page.ts
+++ tests/pages/accounts-overview.page.ts
@@ -18,10 +18,26 @@ export class AccountsOverviewPage extends BasePage {
         this.locatorAvailableAmountColumnHeader = page.getByRole('columnheader', {name:'Available Amount'})
         this.locatorTotalRow = page.getByRole('row', {name:'Total'})
     }
-
     
     async expectAccountsOverviewPage(){
         await expect(this.page).toHaveURL(/overview/)
         await expect(this.locatorAccountsOverviewTitle).toBeVisible()
+    }
+
+    async getAccountNumber(){
+        const accountId = await this.page.locator('#accountTable tbody tr td a').nth(0).innerText()
+        return accountId
+    }
+
+    async expectAccountDetails(accountNumber,expectedBalance,availableAmount) {
+        const row = this.page.locator('#accountTable tbody tr', { hasText: accountNumber.toString() });
+        const balanceCell = row.locator('td:nth-child(2)');
+        const balanceAvailable = row.locator('td:nth-child(3)');
+        await expect(balanceCell).toHaveText(`$${expectedBalance}`);
+        await expect(balanceAvailable).toHaveText(`$${availableAmount}`);
+    }
+
+    async clickAccountNumber(accountNumber){
+        await this.page.getByRole('link', {name:accountNumber}).click()
     }
 }
