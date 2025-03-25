05990c547dd1475ac0a8c4a63e9158a17d950ff9
Thomas <przegralek.thomas@gmail.com>
2025-03-25 22:24:36 +0100

    Ajout de la classe BillPaymentPage : Formulaire de paiement de factures

--- tests/pages/bill-payment.page.ts
+++ tests/pages/bill-payment.page.ts
@@ -13,27 +13,55 @@ export class BillPaymentPage extends BasePage{
     readonly locatorAccountNumberInput : Locator
     readonly locatorVerifyAccountInput : Locator
     readonly locatorAmountInput : Locator
-    readonly locatorFromAccountInput : Locator
+    readonly locatorFromAccount : Locator
     readonly locatorSendPaymentButton : Locator
+    readonly locatorPaymentCompleteTitle : Locator
+    readonly locatorAlertSuccess : Locator
 
     constructor(page:Page){
         super(page)
         //LOCATOR
         this.locatorTransferFundsTitle = this.page.getByRole('heading', {name:'Bill Payment Service'})
-        this.locatorPayeeNameInput = this.page.getByRole('textbox', {name:'Payee Name:'})
-        this.locatorAddressInput = this.page.getByRole('textbox', {name:'Address:'})
-        this.locatorCityInput = this.page.getByRole('textbox', {name:'City:	'})
-        this.locatorStateInput = this.page.getByRole('textbox', {name:'State:'})
-        this.locatorZipcodeInput = this.page.getByRole('textbox', {name:'Zip Code:'})
-        this.locatorPhoneInput = this.page.getByRole('textbox', {name:'Phone #:'})
-        this.locatorAccountNumberInput = this.page.getByRole('textbox', {name:'Account #:'})
-        this.locatorVerifyAccountInput = this.page.getByRole('textbox', {name:'Verify Account #:'})
-        this.locatorAmountInput = this.page.getByRole('textbox', {name:'Amount: $'})
-        this.locatorFromAccountInput = this.page.getByRole('textbox', {name:'From account #:'})
+        this.locatorPayeeNameInput = this.page.locator('[name="payee.name"]')
+        this.locatorAddressInput = this.page.locator('[name="payee.address.street"]')
+        this.locatorCityInput = this.page.locator('[name="payee.address.city"]')
+        this.locatorStateInput = this.page.locator('[name="payee.address.state"]')
+        this.locatorZipcodeInput = this.page.locator('[name="payee.address.zipCode"]')
+        this.locatorPhoneInput = this.page.locator('[name="payee.phoneNumber"]')
+        this.locatorAccountNumberInput = this.page.locator('[name="payee.accountNumber"]')
+        this.locatorVerifyAccountInput = this.page.locator('[name="verifyAccount"]')
+        this.locatorAmountInput = this.page.locator('[name="amount"]')
+        this.locatorFromAccount = this.page.locator('[name="fromAccountId"]')
         this.locatorSendPaymentButton = page.locator('[value="Send Payment"]')
+        this.locatorPaymentCompleteTitle = page.getByRole('heading', {name:'Bill Payment Complete'})
+        this.locatorAlertSuccess = page.locator('#billpayResult > p').nth(0)
     }
 
     async expectTransferFundsPage(){
         await expect(this.page).toHaveURL(/billpay/)
+    }
+
+    async fillPayeeInformation(payee){
+        await this.locatorPayeeNameInput.fill(payee.name)    
+        await this.locatorAddressInput.fill(payee.street)
+        await this.locatorCityInput.fill(payee.city)
+        await this.locatorStateInput.fill(payee.state)
+        await this.locatorZipcodeInput.fill(payee.zipCode)
+        await this.locatorPhoneInput.fill(payee.phoneNumber)
+    }
+
+    async fillBillDetails(amount : string, payeeAccount : string, fromAccount){
+        await this.locatorAccountNumberInput.fill(payeeAccount)
+        await this.locatorVerifyAccountInput.fill(payeeAccount)
+        await this.locatorAmountInput.fill(amount)
+        await this.locatorFromAccount.selectOption(fromAccount)
+    }
+
+    async clickSendPayment(){
+        await this.locatorSendPaymentButton.click()
+    }
+
+    async expectPaymentComplete(amount, payeeName, fromAccount ){
+        await expect(this.locatorAlertSuccess).toContainText(`Bill Payment to ${payeeName} in the amount of $${amount} from account ${fromAccount} was successful.`)
     }
 }
